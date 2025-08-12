9b06038ff3d1d825ebf7a46b459299cc3c63a760
hulusi-ozsir <hulusi.ozsir@hotmail.com>
2025-08-12 21:53:48 +0300

    two

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import { Suspense } from "react";
+import { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
-import { lazy } from "react";
+
 const HomePage = lazy(() => import("../pages/HomePage"));
 const MoviesPage = lazy(() => import("../pages/MoviesPage"));
 const MovieDetailsPage = lazy(() => import("../pages/MovieDetailsPage"));
@@ -8,21 +8,20 @@ const MovieCast = lazy(() => import("../components/MovieCast"));
 const MovieReviews = lazy(() => import("../components/MovieReviews"));
 const NotFoundPage = lazy(() => import("../pages/NotFoundPage"));
 const Navigation = lazy(() => import("../components/Navigation"));
+
 export default function App() {
   return (
-    <>
+    <Suspense fallback={<p>Loading...</p>}>
       <Navigation />
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/movies" element={<MoviesPage />} />
-          <Route path="/movies/:movieId/*" element={<MovieDetailsPage />}>
-            <Route path="cast" element={<MovieCast />} />
-            <Route path="reviews" element={<MovieReviews />} />
-          </Route>
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </Suspense>
-    </>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:movieId/*" element={<MovieDetailsPage />}>
+          <Route path="cast" element={<MovieCast />} />
+          <Route path="reviews" element={<MovieReviews />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
--- src/pages/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage.jsx
@@ -1,24 +1,26 @@
-import { Outlet, useLocation, useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { Outlet, useLocation, useParams, Link, NavLink } from "react-router-dom";
 import css from "./MovieDetailsPage.module.css";
 import { useEffect, useRef, useState } from "react";
 import { fetchMovieDetails, getImageUrl } from "../tmdApi";
-import { NavLink } from "react-router-dom";
 
 export default function MovieDetailsPage() {
   const { movieId } = useParams();
   const location = useLocation();
   const backLink = useRef(location.state?.from || "/movies");
   const [movie, setMovie] = useState(null);
+
   useEffect(() => {
     fetchMovieDetails(movieId).then(setMovie).catch(console.error);
   }, [movieId]);
+
   if (!movie) return <p>Loading movie details...</p>;
+
   return (
     <div className={css.container}>
       <Link to={backLink.current} className={css.backLink}>
         Go Back
       </Link>
+
       <div className={css.details}>
         <img
           src={getImageUrl(movie.poster_path) || "/no-image.png"}
@@ -31,25 +33,28 @@ export default function MovieDetailsPage() {
           <h3>Overview</h3>
           <p>{movie.overview}</p>
           <h3>Genres</h3>
-          <p>{movie.genres.map((g) => g.name).join(",")}</p>
+          <p>{movie.genres.map((g) => g.name).join(", ")}</p>
         </div>
       </div>
+
       <hr />
+
       <div className={css.additional}>
         <p>Additional information:</p>
         <ul>
           <li>
-            <NavLink to={`/movies/${movieId}/cast`} className={css.link}>
+            <NavLink to="cast" className={css.link}>
               Cast
             </NavLink>
           </li>
           <li>
-            <NavLink to={`/movies/${movieId}/reviews`} className={css.link}>
+            <NavLink to="reviews" className={css.link}>
               Reviews
             </NavLink>
           </li>
         </ul>
       </div>
+
       <Outlet />
     </div>
   );
